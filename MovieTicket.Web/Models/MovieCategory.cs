4911e54a5b682f841852cb0d32592457a59bd3db
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:57:49 +0300

    Add MovieCategory join model

---
+++ MovieTicket.Web/Models/MovieCategory.cs
@@ -0,0 +1,13 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class MovieCategory : EntityBase
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
