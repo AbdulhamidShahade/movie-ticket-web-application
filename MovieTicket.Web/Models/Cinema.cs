88a847d76e286ba78b2d900d2e2b06ece490022b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:57:00 +0300

    Add cinema domain model

---
+++ MovieTicket.Web/Models/Cinema.cs
@@ -0,0 +1,11 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class Cinema : EntityBase
+    {
+        public string Name { get; set; }
+        public string Logo { get; set; }
+        public string Description { get; set; }
+    }
+}
