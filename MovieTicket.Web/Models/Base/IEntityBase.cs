232364700b551e99c469c597ea27022b1d8fb244
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:47:58 +0300

    Add IEntityBase.cs

---
+++ MovieTicket.Web/Models/Base/IEntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicket.Web.Models.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
