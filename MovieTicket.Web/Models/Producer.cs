a691bf2ea88afa98eec283ceddb11c7f8ffdd808
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:58:54 +0300

    Add producer domain model

---
+++ MovieTicket.Web/Models/Producer.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieTicket.Web.Models
+{
+    public class Producer : Person
+    {
+        public override string FullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
