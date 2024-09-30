9af6883951ac2314d8a9b2e10e0a4ae0c16e1408
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:57:15 +0300

    Add movie domain model

---
+++ MovieTicket.Web/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class Movie : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
