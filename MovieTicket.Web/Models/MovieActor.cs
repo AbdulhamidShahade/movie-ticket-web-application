4e9cf957b815b09ae606d98db6258f44fee9b320
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:57:35 +0300

    Add MovieActor join model

---
+++ MovieTicket.Web/Models/MovieActor.cs
@@ -0,0 +1,13 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class MovieActor : EntityBase
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
