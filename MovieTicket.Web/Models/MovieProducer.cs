cb2405902e46110921800fec0cebdcc88e95bc65
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:58:01 +0300

    Add MovieProducer join model

---
+++ MovieTicket.Web/Models/MovieProducer.cs
@@ -0,0 +1,12 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class MovieProducer : EntityBase
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int ProducerId { get; set; }
+        public Producer Producer { get; set; }
+    }
+}
