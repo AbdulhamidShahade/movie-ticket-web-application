25b7869600969bcfc92b98dd69846cc8af1d11a9
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:56:32 +0300

    Add category domain model

---
+++ MovieTicket.Web/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class Category : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
