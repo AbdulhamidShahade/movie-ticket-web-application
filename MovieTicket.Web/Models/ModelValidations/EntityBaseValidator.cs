17be2eed7f877ce9c2d67f270ed9941ff05029f9
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:08:39 +0300

    Add entity base validator class

---
+++ MovieTicket.Web/Models/ModelValidations/EntityBaseValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class EntityBaseValidator : AbstractValidator<EntityBase>
+    {
+        public EntityBaseValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(c => c.CreatedAt)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(DateTime.UtcNow);
+
+            RuleFor(u => u.UpdatedAt)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(DateTime.UtcNow);
+        }
+    }
+}
