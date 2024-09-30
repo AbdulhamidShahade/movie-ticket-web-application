20f92076283d89d00ad007d7e5ed12bd97609e0e
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:08:25 +0300

    Add cinema validator class

---
+++ MovieTicket.Web/Models/ModelValidations/CinemaValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MovieTicket.Web.Models;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class CinemaValidator : AbstractValidator<Cinema>
+    {
+        public CinemaValidator()
+        {
+            RuleFor(n => n.Name)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 50);
+
+            RuleFor(l => l.Logo)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+
+            RuleFor(d => d.Description)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+        }
+    }
+}
