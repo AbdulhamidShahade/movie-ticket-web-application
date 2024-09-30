4c8bbfe33163023cb2d652236470c509c0f3ac46
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:10:24 +0300

    Add producer validator class

---
+++ MovieTicket.Web/Models/ModelValidations/ProducerValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MovieTicket.Web.Models;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class ProducerValidator : AbstractValidator<Producer>
+    {
+        public ProducerValidator()
+        {
+
+        }
+    }
+}
