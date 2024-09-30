0802cbb71f698964276a9c5a156324a082453bd3
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:09:13 +0300

    Add movie category join validtor class

---
+++ MovieTicket.Web/Models/ModelValidations/MovieCategoryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class MovieCategoryValidator : AbstractValidator<MovieCategory>  
+    {
+        public MovieCategoryValidator()
+        {
+            RuleFor(m => m.MovieId)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(m => m.Movie)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(c => c.CategoryId)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(c => c.Category)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
