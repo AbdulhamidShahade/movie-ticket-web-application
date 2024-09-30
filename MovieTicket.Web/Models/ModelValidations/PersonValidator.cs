c3202b57dfeada9434fc1c6c5ce8203117a70823
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:10:14 +0300

    Add person validator class

---
+++ MovieTicket.Web/Models/ModelValidations/PersonValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(f => f.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 30);
+                
+
+            RuleFor(l => l.LastName)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 30);
+
+            RuleFor(b => b.BirthDate)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(new DateTime(1900, 1, 1))
+                .NotNull();
+
+            RuleFor(b => b.Bio)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+
+            RuleFor(p => p.PictureUrl)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+        }
+    }
+}
