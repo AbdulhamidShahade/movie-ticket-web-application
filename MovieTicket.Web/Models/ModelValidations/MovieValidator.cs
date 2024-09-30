68287e8f52e2d52170c44d8f42e7e3093e4224e2
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:09:42 +0300

    Add movie validator class

---
+++ MovieTicket.Web/Models/ModelValidations/MovieValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using MovieTicket.Web.Models;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class MovieValidator : AbstractValidator<Movie>
+    {
+        public MovieValidator()
+        {
+            RuleFor(n => n.Name)
+                .NotNull()
+                .NotEmpty()
+                .Length(3, 30);
+
+            RuleFor(d => d.Description)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(1m);
+
+            RuleFor(p => p.PictureUrl)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+
+            RuleFor(s => s.StartDate)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(new DateTime(1900, 1, 1));
+
+
+            RuleFor(e => e.EndDate)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(new DateTime(1900, 1, 1))
+                .GreaterThan(s => s.StartDate);
+        }
+    }
+}
