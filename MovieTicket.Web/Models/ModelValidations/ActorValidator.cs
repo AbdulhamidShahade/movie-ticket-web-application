fd0120d8a64fb777568aa8fdda2be50d43d1cbf8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:08:02 +0300

    Add actor validator class

---
+++ MovieTicket.Web/Models/ModelValidations/ActorValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class ActorValidator : AbstractValidator<Actor>
+    {
+        public ActorValidator()
+        {
+
+        }
+    }
+}
