014270f07d9ec3236c98c154cc33991677a3d686
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:58:22 +0300

    Add OrderDetails join model

---
+++ MovieTicket.Web/Models/OrderDetails.cs
@@ -0,0 +1,16 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class OrderDetails : EntityBase
+    {
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
