54cf7c03df34175a7f6313ac0d162718b3b9fbc7
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:48:17 +0300

    Add EntityBase.cs concrete

---
+++ MovieTicket.Web/Models/Base/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieTicket.Web.Models.Base
+{
+    public class EntityBase : IEntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+            set { }
+        }
+        public DateTime UpdatedAt
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+            set { }
+        }
+    }
+}
