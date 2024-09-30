27408f5c5194e249479d725880deb129647f5c03
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:57:07 +0300

    Add country domain model

---
+++ MovieTicket.Web/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class Country : EntityBase
+    {
+        public string Name { get; set; }
+        public string? Iso { get; set; }
+        public string Nicename { get; set; }
+        public string? Iso3 { get; set; }
+        public int? NumCode { get; set; }
+        public int? PhoneCode { get; set; }
+        public string? PictureUrl { get; set; }
+
+        public List<Producer> Producers { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+}
