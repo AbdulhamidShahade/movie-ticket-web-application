b076285af1045b742a7165600cd7f2701a0bffd5
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 01:58:42 +0300

    Add person abstract

---
+++ MovieTicket.Web/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public abstract class Person : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Bio { get; set; }
+        public string PictureUrl { get; set; }
+        public abstract string FullName();
+    }
+}
