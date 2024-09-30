7ead21cd93afdf9033935841b5dd0d44a2babee1
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:10:02 +0300

    Add order validator class

---
+++ MovieTicket.Web/Models/ModelValidations/OrderValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MovieTicket.Web.Models;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class OrderValidator : AbstractValidator<Order>
+    {
+        public OrderValidator()
+        {
+            RuleFor(e => e.Email)
+                .NotEmpty()
+                .NotNull()
+                .Length(10, 50)
+                .EmailAddress();
+        }
+    }
+}
