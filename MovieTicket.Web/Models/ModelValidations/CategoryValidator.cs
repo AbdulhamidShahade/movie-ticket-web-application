f86c94db8f12da57e7850822ca133733c6e303b8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:08:15 +0300

    Add category validator class

---
+++ MovieTicket.Web/Models/ModelValidations/CategoryValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(n => n.Name)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 50);
+
+
+            RuleFor(d => d.Description)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+
+            RuleFor(p => p.PictureUrl)
+                .NotEmpty()
+                .NotNull()
+                .Length(3, 500);
+        }
+    }
+}
