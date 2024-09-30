e70d7b7c79f4bd9d0809f04f93ccace4268548ac
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-01 02:09:28 +0300

    Add movie producer join validator class

---
+++ MovieTicket.Web/Models/ModelValidations/MovieProducerValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class MovieProducerValidator : AbstractValidator<MovieProducer>
+    {
+        public MovieProducerValidator()
+        {
+            RuleFor(m => m.MovieId)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(m => m.Movie)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(p => p.ProducerId)
+                .NotEmpty()
+                .NotNull()
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(p => p.Producer)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
