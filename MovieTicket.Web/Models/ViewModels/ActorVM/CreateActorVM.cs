1bb38a3386e98943cb7960fb4cb91c3e94607d73
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:18:41 +0300

    Add create actor view model

---
+++ MovieTicket.Web/Models/ViewModels/ActorVM/CreateActorVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.ActorVM
+{
+    public class CreateActorVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Bio { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
