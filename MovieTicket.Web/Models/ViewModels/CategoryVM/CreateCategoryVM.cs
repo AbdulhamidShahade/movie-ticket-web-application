a4d0a5bed4614ba0a92e8f5364f7f05fb0f79696
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:19:15 +0300

    Add create category view model

---
+++ MovieTicket.Web/Models/ViewModels/CategoryVM/CreateCategoryVM.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.CategoryVM
+{
+    public class CreateCategoryVM
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
