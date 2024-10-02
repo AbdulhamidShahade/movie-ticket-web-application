a63b9ff480ceb71139372980f28367cb4a1f798b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:22:30 +0300

    Add shopping cart view model

---
+++ MovieTicket.Web/Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,9 @@
+﻿//namespace MovieTicket.Web.Models.ViewModels
+//{
+//    public class ShoppingCartVM
+//    {
+//        public ShoppingCart ShoppingCart { get; set; }
+//        public decimal ShoppingCartTotal { get; set; }
+
+//    }
+//}
