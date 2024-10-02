5a68f750588cd74ea2e38b444c78d50e39eedb27
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:18:58 +0300

    Add read actor view model

---
+++ MovieTicket.Web/Models/ViewModels/ActorVM/ReadActorVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.ActorVM
+{
+    public class ReadActorVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Bio { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
