e57e7567612bd8b19caf5459903824d7d3402d19
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:21:29 +0300

    Add movie dropdown lists view model

---
+++ MovieTicket.Web/Models/ViewModels/MovieVM/MovieDropdownListsVM.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.MovieVM
+{
+    public class MovieDropdownListsVM
+    {
+        public MovieDropdownListsVM()
+        {
+            Producers = new List<Producer>();
+            Actors = new List<Actor>();
+            Categories = new List<Category>();
+            Cinemas = new List<Cinema>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Actor> Actors { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+    }
+}
