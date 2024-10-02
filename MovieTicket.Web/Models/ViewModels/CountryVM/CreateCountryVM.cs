45045feff57beb1ca923608c89d182f0a76db294
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:20:29 +0300

    Add create country view model

---
+++ MovieTicket.Web/Models/ViewModels/CountryVM/CreateCountryVM.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.CountryVM
+{
+    public class CreateCountryVM
+    {
+        public string Name { get; set; }
+        public string? Iso { get; set; }
+        public string Nicename { get; set; }
+        public string? Iso3 { get; set; }
+        public int? NumCode { get; set; }
+        public int? PhoneCode { get; set; }
+        public string? PictureUrl { get; set; }
+    }
+}
