29b8d3eeb2b59406e99609cff47c515a1a795bf2
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:19:50 +0300

    Add create cinema view model

---
+++ MovieTicket.Web/Models/ViewModels/CinemaVM/CreateCinemaVM.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.CinemaVM
+{
+    public class CreateCinemaVM
+    {
+        public string Name { get; set; }
+        public string Logo { get; set; }
+        public string Description { get; set; }
+    }
+}
