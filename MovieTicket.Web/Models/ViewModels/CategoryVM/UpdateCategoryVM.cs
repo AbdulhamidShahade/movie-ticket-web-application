d937ca3cf3d29cdfe004eb23208c3171bad5dea6
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:19:43 +0300

    Add update category view model

---
+++ MovieTicket.Web/Models/ViewModels/CategoryVM/UpdateCategoryVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.CategoryVM
+{
+    public class UpdateCategoryVM
+    {
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
