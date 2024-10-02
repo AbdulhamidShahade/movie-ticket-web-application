345d5ff1f8450cf3ffdc50feac992a47ba6df42d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:23:20 +0300

    Add shopping cart item domain model

---
+++ MovieTicket.Web/Models/ShoppingCartItem.cs
@@ -0,0 +1,13 @@
+﻿using MovieTicket.Web.Models.Base;
+
+namespace MovieTicket.Web.Models
+{
+    public class ShoppingCartItem : EntityBase
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int Amount { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+    }
+}
