e972cdfa0e31a1c8ac9acba197133f5941272c5e
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:22:07 +0300

    Add read producer view model

---
+++ MovieTicket.Web/Models/ViewModels/ProducerVM/ReadProducerVM.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.ProducerVM
+{
+    public class ReadProducerVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Bio { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
