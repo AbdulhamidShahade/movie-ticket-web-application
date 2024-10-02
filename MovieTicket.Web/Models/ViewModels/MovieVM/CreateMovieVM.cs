16f9a0594861fb56b22e8534cf7e5fb60ef2ab5f
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:21:04 +0300

    Add create movie view model

---
+++ MovieTicket.Web/Models/ViewModels/MovieVM/CreateMovieVM.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.MovieVM
+{
+    public class CreateMovieVM
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<int> CategoryIds { get; set; }
+        public List<int> ActorIds { get; set; }
+        public List<int> ProducerIds { get; set; }
+        public int CinemaId { get; set; }
+    }
+}
