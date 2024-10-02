d159f1a9c5a066f12d9bb54b916af1abafe7f777
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:21:52 +0300

    Add create producer view model

---
+++ MovieTicket.Web/Models/ViewModels/ProducerVM/CreateProducerVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.ProducerVM
+{
+    public class CreateProducerVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Bio { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
