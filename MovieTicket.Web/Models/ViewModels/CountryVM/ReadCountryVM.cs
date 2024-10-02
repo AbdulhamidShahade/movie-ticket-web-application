58bfedab387f6009de4f1dfb50d9226f8ab7245d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:20:49 +0300

    Add read country view model

---
+++ MovieTicket.Web/Models/ViewModels/CountryVM/ReadCountryVM.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.CountryVM
+{
+    public class ReadCountryVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Iso { get; set; }
+        public string Nicename { get; set; }
+        public string? Iso3 { get; set; }
+        public int? NumCode { get; set; }
+        public int? PhoneCode { get; set; }
+        public string? PictureUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
