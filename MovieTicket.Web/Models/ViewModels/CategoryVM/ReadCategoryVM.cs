611b38bb0f519b25c2b1c31881593ac946b6fc79
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:19:34 +0300

    Add read category view model

---
+++ MovieTicket.Web/Models/ViewModels/CategoryVM/ReadCategoryVM.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.CategoryVM
+{
+    public class ReadCategoryVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
