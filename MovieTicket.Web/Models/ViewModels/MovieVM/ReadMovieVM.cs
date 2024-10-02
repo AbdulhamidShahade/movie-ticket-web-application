50d3faa8edfce775040f6b1d436af18ae0549261
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-02 23:21:37 +0300

    Add read movie view model

---
+++ MovieTicket.Web/Models/ViewModels/MovieVM/ReadMovieVM.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieTicket.Web.Models.ViewModels.MovieVM
+{
+    public class ReadMovieVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
