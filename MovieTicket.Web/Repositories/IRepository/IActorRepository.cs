65d124685a3736440b53bf76dd95ad5c56d303df
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:06:30 +0300

    Add IActorRepository contract

---
+++ MovieTicket.Web/Repositories/IRepository/IActorRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface IActorRepository : IBaseRepository<Actor>
+    {
+        Task<List<Movie>> GetMoviesByActorId(int actorId);
+        Task<List<Actor>> GetActorsByCountryIdAsync(int countryId);
+    }
+}
