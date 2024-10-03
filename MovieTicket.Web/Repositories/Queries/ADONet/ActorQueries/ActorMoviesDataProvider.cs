a11b8cc1eb94a6b02d67eaff1f4b7bd12aac7842
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:12:32 +0300

    Add ActorMovies data provider model

---
+++ MovieTicket.Web/Repositories/Queries/ADONet/ActorQueries/ActorMoviesDataProvider.cs
@@ -0,0 +1,48 @@
+﻿using MovieTicket.Web.Models.ViewModels.MovieVM;
+using MovieTicket.Web.Settings;
+using System.Data.SqlClient;
+
+namespace MovieTicketWebApplication.Repositories.Queries.ADONet.ActorQueries
+{
+    public class ActorMoviesDataProvider
+    {
+        public List<ReadMovieVM> GetMoviesByActorId(int actorId, int quantity)
+        {
+            List<ReadMovieVM> movies = new List<ReadMovieVM>();
+            SqlConnection conn = new SqlConnection(Global.ConnectionString);
+            string query = $"SELECT top {quantity} * from Movies m inner join MoviesActors ma on (m.id = ma.movieId) where ma.actorId = @actorId";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@actorId", actorId);
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    movies.Add(new ReadMovieVM
+                    {
+                        Id = Convert.ToInt16(reader["Id"]),
+                        Name = Convert.ToString(reader["Name"]),
+                        Description = Convert.ToString(reader["Description"]),
+                        Price = Convert.ToDecimal(reader["Price"]),
+                        Length = Convert.ToInt16(reader["Length"]),
+                        PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                        StartDate = DateOnly.FromDateTime(Convert.ToDateTime(reader["StartDate"])),
+                        EndDate = DateOnly.FromDateTime(Convert.ToDateTime(reader["EndDate"])),
+                    });
+                }
+                reader.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return movies;
+        }
+    }
+}
