0cb61b4ed1096943800688ffded058c8fd72b6cc
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:10:11 +0300

    Add UserRepository concrete

---
+++ MovieTicket.Web/Repositories/Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Web.Models;
+
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        public UserManager<ApplicationUser> _userManager;
+
+        public UserRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<List<ApplicationUser>> GetAll()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+    }
+}
