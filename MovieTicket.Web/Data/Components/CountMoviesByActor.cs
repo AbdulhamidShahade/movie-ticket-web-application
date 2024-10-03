bc7dd222cd5887c052e41196c0cd4a7a37f76f71
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:16:40 +0300

    Add count movies by actor component

---
+++ MovieTicket.Web/Data/Components/CountMoviesByActor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicket.Web.Data.Components
+{
+    public class CountMoviesByActor : ViewComponent
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public CountMoviesByActor(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public IViewComponentResult Invoke(int actorId)
+        {
+            var count = _movieRepository.GetMoviesCountByActorId(actorId);
+
+            return View(count);
+        }
+    }
+}
