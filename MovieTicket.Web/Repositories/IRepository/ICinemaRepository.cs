cbd75c75796bcb8e474ea81a685f2e50d14ef561
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:07:08 +0300

    Add ICinemaRepository contract

---
+++ MovieTicket.Web/Repositories/IRepository/ICinemaRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface ICinemaRepository : IBaseRepository<Cinema>
+    {
+        Task<List<Movie>> GetMoviesByCinemaId(int cinemaId);
+    }
+}
