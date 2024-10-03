ef06929120b669c86493e1820c680700eea525b7
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:14:24 +0300

    Add application settings model

---
+++ MovieTicket.Web/Settings/Global.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieTicket.Web.Settings
+{
+    public static class Global
+    {
+        public static string ConnectionString = "Server=.;Database=movie-ticket-web-application;Trusted_Connection=True;";
+    }
+}
