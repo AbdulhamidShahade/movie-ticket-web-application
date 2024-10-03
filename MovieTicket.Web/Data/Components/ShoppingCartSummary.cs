bc1d0548e90c74d50ca383770478b2c7bddb9d7a
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:17:08 +0300

    Add shopping cart summary component

---
+++ MovieTicket.Web/Data/Components/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+
+
+//namespace MovieTicket.Web.Data.Components
+//{
+//    public class ShoppingCartSummary : ViewComponent
+//    {
+//        private readonly ShoppingCart _shoppingCart;
+//        public ShoppingCartSummary(ShoppingCart shoppingCart)
+//        {
+//            _shoppingCart = shoppingCart;
+//        }
+
+//        public async Task<IViewComponentResult> Invoke()
+//        {
+//            var items = await _shoppingCart.GetAllShoppingCartItemsAsync();
+
+//            return View(items.Count);
+//        }
+//    }
+//}
