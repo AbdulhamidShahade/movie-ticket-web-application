fc9288f28d63e934ace7db6d0c51fd0bad4ebfa6
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:12:03 +0300

    Add user roles constants

---
+++ MovieTicket.Web/Data/Statics/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieTicket.Web.Data.Statics
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
