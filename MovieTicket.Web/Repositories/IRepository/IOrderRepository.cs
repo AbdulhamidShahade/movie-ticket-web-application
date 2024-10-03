ded13316e1dd81a91f90bc484d564318f83b34b8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:07:38 +0300

    Add IOrderRepository contract

---
+++ MovieTicket.Web/Repositories/IRepository/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+using MovieTicket.Web.Models;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface IOrderRepository
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, int userId, string EmailAddress);
+        Task<List<Order>> GetOrdersAsync(int userId, string userRole);
+    }
+}
