24dc77449d639cd51374ac993b6a20e3b0b49653
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:15:37 +0300

    Add notification halper model

---
+++ MovieTicket.Web/Helpers/NotificationHalper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieTicket.Web.Helpers
+{
+    public static class NotificationHalper
+    {
+        public static void SetNotification(Controller controller, string type, string message)
+        {
+            controller.TempData[type] = message;
+        }
+    }
+}
