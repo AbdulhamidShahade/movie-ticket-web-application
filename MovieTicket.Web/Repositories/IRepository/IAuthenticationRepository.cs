fb76f3128caf0e7af82276cf9b36a26d1b30887d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:06:45 +0300

    Add IAuthentication contract

---
+++ MovieTicket.Web/Repositories/IRepository/IAuthenticationRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MovieTicket.Models.ViewModels.AuthenticationVM;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface IAuthenticationRepository
+    {
+        Task<bool> Register(RegisterVM registerVM);
+        Task<bool> Login(LoginVM loginVM);
+        Task LogoutAsync();
+    }
+}
