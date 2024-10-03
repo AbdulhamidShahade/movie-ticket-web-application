afc413b73be1a6ab57bf3fb6509e4ba771dd155c
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:06:58 +0300

    Add ICategoryRepository contract

---
+++ MovieTicket.Web/Repositories/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface ICategoryRepository : IBaseRepository<Category>
+    {
+        Task<List<Movie>> GetMoviesByCategoryId(int movieId);
+    }
+}
