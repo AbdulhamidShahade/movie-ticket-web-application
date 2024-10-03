e9578adfe771eb0ba00f94509bc6d1ed13a6e331
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:11:33 +0300

    Add register view model

---
+++ MovieTicket.Web/Models/ViewModels/AuthenticationVM/RegisterVM.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieTicket.Models.ViewModels.AuthenticationVM
+{
+    public class RegisterVM
+    {
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
