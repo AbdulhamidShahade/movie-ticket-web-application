5b8c6df8142edaeb678c9cdd531afc4e3da7b344
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:11:24 +0300

    Add login view model

---
+++ MovieTicket.Web/Models/ViewModels/AuthenticationVM/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieTicket.Models.ViewModels.AuthenticationVM
+{
+    public class LoginVM
+    {
+        public string EmailAddress { get; set; }
+        public string Pasword { get; set; }
+    }
+}
