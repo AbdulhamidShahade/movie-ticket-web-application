5203b1a75bd20e583eed6b5e7c8dad1a43e460ef
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:09:17 +0300

    Add CategoryRepository concrete

---
+++ MovieTicket.Web/Repositories/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Web.Data;
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicket.Web.Repositories.Repository
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Movie>> GetMoviesByCategoryId(int movieId)
+        {
+            var movies = await _context.MoviesCategories.Where(m => m.MovieId == movieId)
+                .Select(m => m.Movie).ToListAsync();
+
+            return movies;
+        }
+    }
+}
