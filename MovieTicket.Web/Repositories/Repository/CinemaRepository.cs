5ac9c19c05d4fe87698d20301d77f0d4799c80f3
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:09:26 +0300

    Add CinemaRepository concrete

---
+++ MovieTicket.Web/Repositories/Repository/CinemaRepository.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Web.Data;
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicket.Web.Repositories.Repository
+{
+    public class CinemaRepository : BaseRepository<Cinema>, ICinemaRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CinemaRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Movie>> GetMoviesByCinemaId(int cinemaId)
+        {
+            var movies = await _context.Movies.Where(ci => ci.CinemaId == cinemaId).ToListAsync();
+
+            return movies;
+        }
+    }
+}
