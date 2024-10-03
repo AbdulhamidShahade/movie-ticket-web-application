edf9c20e9b758777fa428a51159a74c339891a7b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:17:20 +0300

    Add total movies component

---
+++ MovieTicket.Web/Data/Components/TotalMovie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicket.Web.Data.Components
+{
+    public class TotalMovie : ViewComponent
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public TotalMovie(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public IViewComponentResult Invoke(int categoryId)
+        {
+            var moviesCount = _movieRepository.GetMoviesByCategoryId(categoryId);
+
+            return View(moviesCount);
+        }
+    }
+}
