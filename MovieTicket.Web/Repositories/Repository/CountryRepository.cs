6bc95998b3f8f4edfa819a7b8d39730479e9f3b9
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:09:34 +0300

    Add CountryRepository concrete

---
+++ MovieTicket.Web/Repositories/Repository/CountryRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+using MovieTicket.Web.Data;
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicket.Web.Repositories.Repository
+{
+    public class CountryRepository : BaseRepository<Country>, ICountryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CountryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+
+}
