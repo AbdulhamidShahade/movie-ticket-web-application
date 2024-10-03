b3799420a792aae8dde1c38173b53fa168e26f38
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:07:49 +0300

    Add IProducerRepository contract

---
+++ MovieTicket.Web/Repositories/IRepository/IProducerRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface IProducerRepository : IBaseRepository<Producer>
+    {
+        
+    }
+}
