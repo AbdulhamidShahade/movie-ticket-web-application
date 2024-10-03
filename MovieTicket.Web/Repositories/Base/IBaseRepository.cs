b15424a8d6bb14d94eb4d4680e0f88afd08c3a41
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:08:18 +0300

    Add IBaseRepository contract

---
+++ MovieTicket.Web/Repositories/Base/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace MovieTicket.Web.Repositories.Base
+{
+    public interface IBaseRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includeProperties);
+        Task<T> GetAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includeProperties);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+    }
+}
