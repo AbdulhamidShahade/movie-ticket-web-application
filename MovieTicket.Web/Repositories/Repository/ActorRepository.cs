9ae0f9428438aa2b3c40e40c19f5cd24ee0d7dcd
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:08:51 +0300

    Add ActorRepository concrete

---
+++ MovieTicket.Web/Repositories/Repository/ActorRepository.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Web.Data;
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Repositories.Base;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicket.Web.Repositories.Repository
+{
+    public class ActorRepository : BaseRepository<Actor>, IActorRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ActorRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Actor>> GetActorsByCountryIdAsync(int countryId)
+        {
+            var actors = await _context.Actors.Where(c => c.CountryId == countryId).ToListAsync();
+
+            return actors;
+        }
+
+        public async Task<List<Movie>> GetMoviesByActorId(int actorId)
+        {
+            List<Movie> movies = await _context.MoviesActors.Where(a => a.ActorId == actorId).Select(m => m.Movie).ToListAsync();
+
+            return movies;
+        }
+    }
+}
+   
