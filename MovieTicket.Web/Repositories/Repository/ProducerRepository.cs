b27e6a9c492392da9768b403708b438033c94293
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:10:00 +0300

    Add producerRepository concrete

---
+++ MovieTicket.Web/Repositories/Repository/ProducerRepository.cs
@@ -0,0 +1,25 @@
+﻿
+//using MovieTicket.Web.Data;
+//using MovieTicket.Web.Models;
+//using MovieTicket.Web.Repositories.Base;
+//using MovieTicket.Web.Repositories.IRepository;
+
+//namespace MovieTicket.Web.Repositories.Repository
+//{
+//    public class ProducerRepository : BaseRepository<Producer>, IProducerRepository
+//    {
+//        private readonly ApplicationDbContext _context;
+
+//        public ProducerRepository(ApplicationDbContext context) : base(context)
+//        {
+//            _context = context;
+//        }
+
+//        public async Task<List<Movie>> GetMoviesByProducerId(int producerId)
+//        {
+//            var movies = await _context.MoviesProducers.Where(p => p.ProducerId == producerId).Select(m => m.Movie).ToListAsync();
+
+//            return movies;
+//        }
+//    }
+//}
