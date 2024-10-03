513fbc2c0dd0bcd8bd38c62ec869622b7dd24f6a
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-03 18:07:29 +0300

    Add IMovieRepository contract

---
+++ MovieTicket.Web/Repositories/IRepository/IMovieRepository.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using MovieTicket.Web.Models;
+using MovieTicket.Web.Models.ViewModels.MovieVM;
+using MovieTicket.Web.Repositories.Base;
+
+namespace MovieTicket.Web.Repositories.IRepository
+{
+    public interface IMovieRepository : IBaseRepository<Movie>
+    {
+        Task<List<Actor>> GetActorsByMovieId(int movieId);
+        Task<List<Category>> GetCategoriesByMovieId(int movieId);
+        Task<List<Producer>> GetProducersByMovieId(int movieId);
+        Task<Cinema> GetCinemaByMovieId(int id);
+        Task<MovieDropdownListsVM> GetMovieDropDownLists();
+        Task<bool> CreateMovieAsync(CreateMovieVM createMovieVM);
+        Task<bool> UpdateMovieAsync(UpdateMovieVM updateMovieVM);
+
+        List<Movie> GetMoviesByActorId(int actorId);
+
+
+        Task<List<Movie>> GetMoviesByProducerIdAsync(int producerId);
+        List<Movie> GetMoviesByProducerId(int producerId);
+
+        int GetMoviesCountByActorId(int actorId);
+
+        List<Movie> GetMoviesByCategoryId(int categoryId);
+
+  
+
+
+    }
+}
