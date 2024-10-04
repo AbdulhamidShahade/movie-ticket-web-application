18512a20a92b27b2b5cefef66597812213f137c7
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-04 23:21:36 +0300

    Add users controller

---
+++ MovieTicket.Web/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTicket.Web.Repositories.IRepository;
+
+namespace MovieTicketWebApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAll();
+
+            return View(users);
+        }
+    }
+}
