fc84350b886feb4ad4e03f9a763c2df86a0b7935
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-09 00:04:21 +0300

    Add delete actor js file

---
+++ MovieTicket.Web/wwwroot/js/Actors/delete.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var output = document.getElementById('ActorPicturePreview');
+    output.src = $("#PictureUrl").val();
+})
+
+$("#PictureUrl").on("change", function () {
+    var output = document.getElementById('ActorPicturePreview');
+    output.src = $(this).val();
+})
+
