bf84cd9b949c471dfb0c982242a884c7873f2866
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-10 23:43:34 +0300

    Add migrations

--- MovieTicket.Web/Migrations/20241007173802_configurateUpdateEntityWithNullByDefault.Designer.cs
+++ MovieTicket.Web/Migrations/20241007173802_configurateUpdateEntityWithNullByDefault.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTicket.Web.Data;
 
@@ -11,9 +12,11 @@ using MovieTicket.Web.Data;
 namespace MovieTicket.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241007173802_configurateUpdateEntityWithNullByDefault")]
+    partial class configurateUpdateEntityWithNullByDefault
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
