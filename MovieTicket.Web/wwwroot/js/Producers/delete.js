1bb7dfee39b6239ad8edd5ec9d8a6f708d20bb28
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-10 23:35:57 +0300

    Add delete producer js file

---
+++ MovieTicket.Web/wwwroot/js/Producers/delete.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var output = document.getElementById('ProducerPicturePreview');
+    output.src = $("#PictureUrl").val();
+})
+
+$("#PictureUrl").on("change", function () {
+    var output = document.getElementById('ProducerPicturePreview');
+    output.src = $(this).val();
+})
