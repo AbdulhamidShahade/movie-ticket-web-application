54fc1d573fea134abc9b2ff3b0d67c6baa511ae8
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-10 23:35:15 +0300

    Add update movie js file

---
+++ MovieTicket.Web/wwwroot/js/Movies/update.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var output = document.getElementById("ImageUrlPreview");
+    output.src = $("#PictureUrl").val();
+});
+
+$("#PictureUrl").on("input", function () {
+    var imageUrl = $(this).val();
+    $("#ImageUrlPreview").attr("src", imageUrl);
+});
