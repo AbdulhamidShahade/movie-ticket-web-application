fe5fcd721a683cd43db9a9b4e9e07fbc28a0ff29
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-10 23:39:59 +0300

    Add MovieCategories data provider

---
+++ MovieTicket.Web/Repositories/Queries/ADONet/CategoryQueries/MovieCategoriesDataProvider.cs
@@ -0,0 +1,52 @@
+﻿using MovieTicket.Web.Models.ViewModels.CategoryVM;
+using MovieTicket.Web.Settings;
+using System.Data.SqlClient;
+
+namespace MovieTicket.Web.Repositories.Queries.ADONet.CategoryQueries
+{
+    public class MovieCategoriesDataProvider
+    {
+        public List<ReadCategoryVM> GetCategoriesByMovieId(int movieId, int quantity)
+        {
+            List<ReadCategoryVM> categories = new List<ReadCategoryVM>();
+            SqlConnection conn = new SqlConnection(Global.ConnectionString);
+
+            string query = $"SELECT top {quantity} * from Categories c inner join MoviesCategories mc on (c.Id = mc.CategoryId) where MovieId = @movieId";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@movieId", movieId);
+
+            try
+            {
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    categories.Add(new ReadCategoryVM()
+                    {
+                        Id = Convert.ToInt16(reader["Id"]),
+                        Name = Convert.ToString(reader["Name"]),
+                        Description = Convert.ToString(reader["Description"]),
+                        PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                        CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+                    });
+                }
+
+                reader.Close();
+                conn.Close();
+            }catch(Exception ex)
+            {
+                conn.Close();
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return categories;
+        }
+    }
+}
