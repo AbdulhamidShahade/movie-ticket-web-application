9d74576b7432c145008a2961bfb1e99b79fb0f86
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-12 23:43:43 +0300

    Update actor view models

--- MovieTicket.Web/Models/ViewModels/ActorVM/CreateActorVM.cs
+++ MovieTicket.Web/Models/ViewModels/ActorVM/CreateActorVM.cs
@@ -4,8 +4,9 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateOnly BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string Bio { get; set; }
         public string PictureUrl { get; set; }
+        public int CountryId { get; set; }
     }
 }
--- MovieTicket.Web/Models/ViewModels/ActorVM/ReadActorVM.cs
+++ MovieTicket.Web/Models/ViewModels/ActorVM/ReadActorVM.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateOnly BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string Bio { get; set; }
         public string PictureUrl { get; set; }
         public int CountryId { get; set; }
-        public DateOnly CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
