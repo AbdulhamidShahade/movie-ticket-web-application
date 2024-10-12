3e4c92810d153191ce687d4b459961738eb44591
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-12 23:49:04 +0300

    Add country view model validator

---
+++ MovieTicket.Web/Models/ModelValidations/CountryViewModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MovieTicket.Web.Models.ViewModels.CountryVM;
+
+namespace MovieTicket.Web.Models.ModelValidations
+{
+    public class CountryViewModelValidator : AbstractValidator<ReadCountryVM>
+    {
+        public CountryViewModelValidator()
+        {
+            RuleFor(p => p.PhoneCode)
+                .NotEmpty()
+                .WithMessage("Phone Code");
+        }
+    }
+}
