b6925403dea4fb79b5403d85e3f4ae7d6ee81f17
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-14 17:54:04 +0300

    Add MySql DbContext to DI container

--- MovieTicket.Web/Program.cs
+++ MovieTicket.Web/Program.cs
@@ -15,9 +15,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
+//});
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
+    options.UseMySql(builder.Configuration.GetConnectionString("MySQL"), new MySqlServerVersion(new Version(10, 4, 30)));
 });
 
 builder.Services.AddScoped<IActorRepository, ActorRepository>();
