268ff8d3cd6b4325f8dc68028970e121713da1fe
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-16 23:47:35 +0300

    Update actor queries

--- MovieTicket.Web/Repositories/Queries/ADONet/ActorQueries/CountryActorsDataProvider.cs
+++ MovieTicket.Web/Repositories/Queries/ADONet/ActorQueries/CountryActorsDataProvider.cs
@@ -1,57 +1,106 @@
 ﻿using MovieTicket.Web.Models.ViewModels.ActorVM;
 using MovieTicket.Web.Settings;
+using MySql.Data.MySqlClient;
 using System.Data.SqlClient;
 
 namespace MovieTicket.Web.Repositories.Queries.ADONet.ActorQueries
 {
     public class CountryActorsDataProvider
     {
-        public List<ReadActorVM> GetActorsByCountryId(int countryId, int quantity)
+        public List<ReadActorVM> GetActorsByCountryId(int countryId, int quantity, string Provider)
         {
             List<ReadActorVM> actors = new List<ReadActorVM>();
 
-            SqlConnection conn = new SqlConnection(Global.ConnectionString);
-
-            string query = $"SELECT top {quantity}* from Actors where CountryId = @countryId";
-
-            SqlCommand cmd = new SqlCommand(query, conn);
-
-            cmd.Parameters.AddWithValue("@countryId", countryId);
-
-            try
+            if (Provider == "SQL")
             {
-                conn.Open();
+                SqlConnection conn = new SqlConnection(Global.ConnectionString);
 
-                SqlDataReader reader = cmd.ExecuteReader();
+                string query = $"SELECT top {quantity}* from Actors where CountryId = @countryId";
 
-                while (reader.Read())
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@countryId", countryId);
+
+                try
                 {
-                    actors.Add(new ReadActorVM()
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
                     {
-                        Id = Convert.ToInt16(reader["Id"]),
-                        FirstName = Convert.ToString(reader["FirstName"]),
-                        LastName = Convert.ToString(reader["LastName"]),
-                        BirthDate = Convert.ToDateTime(reader["BirthDate"]),
-                        Bio = Convert.ToString(reader["Bio"]),
-                        PictureUrl = Convert.ToString(reader["PictureUrl"]),
-                        CountryId = Convert.ToInt16(reader["CountryId"]),
-                        CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
-                    });
+                        actors.Add(new ReadActorVM()
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            FirstName = Convert.ToString(reader["FirstName"]),
+                            LastName = Convert.ToString(reader["LastName"]),
+                            BirthDate = Convert.ToDateTime(reader["BirthDate"]),
+                            Bio = Convert.ToString(reader["Bio"]),
+                            PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                            CountryId = Convert.ToInt16(reader["CountryId"]),
+                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
+                        });
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+                catch
+                {
+                    conn.Close();
+                }
+                finally
+                {
+                    conn.Close();
                 }
 
-                reader.Close();
-                conn.Close();
+                return actors;
             }
-            catch
+            else
             {
-                conn.Close();
-            }
-            finally
-            {
-                conn.Close();
-            }
+                MySqlConnection conn = new MySqlConnection(Global.ConnectionString);
 
-            return actors;
+                string query = $"SELECT * from Actors where CountryId = @countryId LIMIT {quantity}";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@countryId", countryId);
+
+                try
+                {
+                    conn.Open();
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        actors.Add(new ReadActorVM()
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            FirstName = Convert.ToString(reader["FirstName"]),
+                            LastName = Convert.ToString(reader["LastName"]),
+                            BirthDate = Convert.ToDateTime(reader["BirthDate"]),
+                            Bio = Convert.ToString(reader["Bio"]),
+                            PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                            CountryId = Convert.ToInt16(reader["CountryId"]),
+                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
+                        });
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+                catch
+                {
+                    conn.Close();
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+                return actors;
+            }
         }
     }
 }
--- MovieTicket.Web/Repositories/Queries/ADONet/ActorQueries/MovieActorsDataProvider.cs
+++ MovieTicket.Web/Repositories/Queries/ADONet/ActorQueries/MovieActorsDataProvider.cs
@@ -1,54 +1,105 @@
 ﻿using MovieTicket.Web.Models;
 using MovieTicket.Web.Models.ViewModels.ActorVM;
 using MovieTicket.Web.Settings;
+using MySql.Data.MySqlClient;
 using System.Data.SqlClient;
 
 namespace MovieTicket.Web.Repositories.Queries.ADONet.ActorQueries
 {
     public class MovieActorsDataProvider
     {
-        public List<ReadActorVM> GetActorsByMovieId(int movieId, int quantity)
+        public List<ReadActorVM> GetActorsByMovieId(int movieId, int quantity, string Provider)
         {
             List<ReadActorVM> actors = new List<ReadActorVM>();
-            SqlConnection conn = new SqlConnection(Global.ConnectionString);
 
-            string query = $"SELECT top {quantity} * from Actors a inner join MoviesActors ma on (a.Id = ma.ActorId) where MovieId = @movieId";
-
-            SqlCommand cmd = new SqlCommand(query, conn);
-
-            cmd.Parameters.AddWithValue("@movieId", movieId);
-
-            try
+            if(Provider == "SQL")
             {
-                conn.Open();
+                SqlConnection conn = new SqlConnection(Global.ConnectionString);
 
-                SqlDataReader reader = cmd.ExecuteReader();
+                string query = $"SELECT top {quantity} * from Actors a inner join MoviesActors ma on (a.Id = ma.ActorId) where MovieId = @movieId";
 
-                while (reader.Read())
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@movieId", movieId);
+
+                try
                 {
-                    actors.Add(new ReadActorVM()
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
                     {
-                        Id = Convert.ToInt16(reader["Id"]),
-                        FirstName = Convert.ToString(reader["FirstName"]),
-                        LastName = Convert.ToString(reader["LastName"]),
-                        BirthDate = Convert.ToDateTime(reader["BirthDate"]),
-                        Bio = Convert.ToString(reader["Bio"]),
-                        PictureUrl = Convert.ToString(reader["PictureUrl"]),
-                        CountryId = Convert.ToInt16(reader["CountryId"]),
-                        CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
-                    });
+                        actors.Add(new ReadActorVM()
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            FirstName = Convert.ToString(reader["FirstName"]),
+                            LastName = Convert.ToString(reader["LastName"]),
+                            BirthDate = Convert.ToDateTime(reader["BirthDate"]),
+                            Bio = Convert.ToString(reader["Bio"]),
+                            PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                            CountryId = Convert.ToInt16(reader["CountryId"]),
+                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
+                        });
+                    }
+
+                    reader.Close();
+                    conn.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            else
+            {
+                MySqlConnection conn = new MySqlConnection(Global.ConnectionString);
+
+                string query = $"SELECT * From Actors a inner join MoviesActors ma on(a.Id = ma.ActorId) where ma.MovieId = @movieId LIMIT {quantity}";
+            
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@movieId", movieId);
+
+                try
+                {
+                    conn.Open();
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        actors.Add(new ReadActorVM()
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            FirstName = Convert.ToString(reader["FirstName"]),
+                            LastName = Convert.ToString(reader["LastName"]),
+                            BirthDate = Convert.ToDateTime(reader["BirthDate"]),
+                            Bio = Convert.ToString(reader["Bio"]),
+                            PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                            CountryId = Convert.ToInt16(reader["CountryId"]),
+                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"])
+                        });
+                    }
+
+                    reader.Close();
+                    conn.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                }
+                finally
+                {
+                    conn.Close();
                 }
 
-                reader.Close();
-                conn.Close();
-
-            }catch (Exception ex)
-            {
-                conn.Close();
-            }
-            finally
-            {
-                conn.Close();
             }
 
             return actors;
