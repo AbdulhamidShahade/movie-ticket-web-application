1740ea4c3570db0c826b173bfd14b058bac2ac28
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-16 23:47:49 +0300

    Update category queries

--- MovieTicket.Web/Repositories/Queries/ADONet/CategoryQueries/MovieCategoriesDataProvider.cs
+++ MovieTicket.Web/Repositories/Queries/ADONet/CategoryQueries/MovieCategoriesDataProvider.cs
@@ -1,50 +1,97 @@
 ﻿using MovieTicket.Web.Models.ViewModels.CategoryVM;
 using MovieTicket.Web.Settings;
+using MySql.Data.MySqlClient;
 using System.Data.SqlClient;
 
 namespace MovieTicket.Web.Repositories.Queries.ADONet.CategoryQueries
 {
     public class MovieCategoriesDataProvider
     {
-        public List<ReadCategoryVM> GetCategoriesByMovieId(int movieId, int quantity)
+        public List<ReadCategoryVM> GetCategoriesByMovieId(int movieId, int quantity, string Provider)
         {
             List<ReadCategoryVM> categories = new List<ReadCategoryVM>();
-            SqlConnection conn = new SqlConnection(Global.ConnectionString);
 
-            string query = $"SELECT top {quantity} * from Categories c inner join MoviesCategories mc on (c.Id = mc.CategoryId) where MovieId = @movieId";
-
-            SqlCommand cmd = new SqlCommand(query, conn);
-
-            cmd.Parameters.AddWithValue("@movieId", movieId);
-
-            try
+            if(Provider == "SQL")
             {
-                conn.Open();
+                SqlConnection conn = new SqlConnection(Global.ConnectionString);
 
-                SqlDataReader reader = cmd.ExecuteReader();
+                string query = $"SELECT top {quantity} * from Categories c inner join MoviesCategories mc on (c.Id = mc.CategoryId) where MovieId = @movieId";
 
-                while (reader.Read())
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@movieId", movieId);
+
+                try
                 {
-                    categories.Add(new ReadCategoryVM()
-                    {
-                        Id = Convert.ToInt16(reader["Id"]),
-                        Name = Convert.ToString(reader["Name"]),
-                        Description = Convert.ToString(reader["Description"]),
-                        PictureUrl = Convert.ToString(reader["PictureUrl"]),
-                        CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
-                    });
-                }
+                    conn.Open();
 
-                reader.Close();
-                conn.Close();
-            }catch(Exception ex)
-            {
-                conn.Close();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        categories.Add(new ReadCategoryVM()
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            Name = Convert.ToString(reader["Name"]),
+                            Description = Convert.ToString(reader["Description"]),
+                            PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+                        });
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                }
+                finally
+                {
+                    conn.Close();
+                }
             }
-            finally
+            else
             {
-                conn.Close();
+                MySqlConnection conn = new MySqlConnection(Global.ConnectionString);
+
+                string query = $"SELECT * from Categories c inner join MoviesCategories mc on(c.Id = mc.CategoryId) where MovieId = @movieId LIMIT {quantity}";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@movieId", movieId);
+
+                try
+                {
+                    conn.Open();
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        categories.Add(new ReadCategoryVM()
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            Name = Convert.ToString(reader["Name"]),
+                            Description = Convert.ToString(reader["Description"]),
+                            PictureUrl = Convert.ToString(reader["PictureUrl"]),
+                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+                        });
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                }
+                finally
+                {
+                    conn.Close();
+                }
             }
+            
 
             return categories;
         }
