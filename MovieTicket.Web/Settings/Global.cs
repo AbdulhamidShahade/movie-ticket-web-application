7cd1bd242d5a95e8fc79e267915d6551837b49f6
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-16 23:49:16 +0300

    Update global class

--- MovieTicket.Web/Settings/Global.cs
+++ MovieTicket.Web/Settings/Global.cs
@@ -2,6 +2,6 @@
 {
     public static class Global
     {
-        public static string ConnectionString = "Server=.;Database=MovieTicketDB;Trusted_Connection=True;";
+        public static string ConnectionString = "Server=198.12.238.136;Port=3306;Database=MovieTicketDB;UID=Abdulhamid;Password=Kaito1412@;";
     }
 }
