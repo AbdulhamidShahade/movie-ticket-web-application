3f2bf03e60a54c5d3336eb36c9c892727817f6b5
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-20 02:52:17 +0300

    Update js files

--- MovieTicket.Web/wwwroot/js/Movies/userIndex.js
+++ MovieTicket.Web/wwwroot/js/Movies/userIndex.js
@@ -10,9 +10,9 @@ function showMovieData(movieName, movieDescription, startDate, endDate, movieId,
     document.getElementById('movie-bio').innerHTML = "<strong>Description: </strong> " + movieDescription;
     document.getElementById('movie-start-date').innerHTML = "<strong>Start Date:</strong> " + startDate;
     document.getElementById('movie-end-date').innerHTML = "<strong>End Date:</strong> " + endDate;
-    document.getElementById('movie-details').setAttribute('href', `http://movieticketwebapplication-dev.us-east-1.elasticbeanstalk.com/Movies/Details/${movieId}`);
+    document.getElementById('movie-details').setAttribute('href', `https://movieland24.com/Movies/Details/${movieId}`);
     document.getElementById('movie-picture').setAttribute('src', moviePicture);
-    document.getElementById('shopping-cart').setAttribute('href', `http://movieticketwebapplication-dev.us-east-1.elasticbeanstalk.com/Orders/AddItemToShoppingCart/${movieId}`);
+    document.getElementById('shopping-cart').setAttribute('href', `https://movieland24.com/Orders/AddItemToShoppingCart/${movieId}`);
     document.getElementById('shopping-cart').innerHTML = `<i class="bi bi-cart-plus"></i> Add to Cart (Price ${price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })})`;
     document.getElementById('cinema-name').innerHTML = "<strong>Cinema: </strong>" + cinemaName;
     document.getElementById('movie-category').innerText = categoryNames;
