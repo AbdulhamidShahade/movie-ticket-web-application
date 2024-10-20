5045be32374a78e380eb71b62f20a37b84ae474d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-20 14:12:51 +0300

    Update Program.cs

--- MovieTicket.Web/Program.cs
+++ MovieTicket.Web/Program.cs
@@ -15,19 +15,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
-//});
-
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseMySql(builder.Configuration.GetConnectionString("MySQL"), new MySqlServerVersion(new Version(10, 4, 30)),mysqlOptions =>
-        {
-            mysqlOptions.EnableRetryOnFailure();
-        });
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
 });
+
+
 
 builder.Services.AddScoped<IActorRepository, ActorRepository>();
 builder.Services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
